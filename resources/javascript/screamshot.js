1961a5be96b467f07b00d80f8181ffe76a0598f6
mathhulk <no-reply@mathhulk.com>
2019-01-27 12:02:10 -0800

    Update screamshot.js

--- resources/javascript/screamshot.js
+++ resources/javascript/screamshot.js
@@ -1,11 +1,9 @@
 $(document).ready(function( ) {
-	
 	new ClipboardJS(".copy");
 	
-	$("input").val("https://pnrtscr.com/" + Math.random( ).toString(36).substr(2, 6));
+	$("input").val( "https://pnrtscr.com/" + Math.random( ).toString(36).substr(2, 6) );
 	
 	$(document).on("click", ".generate", function( ) {
-		$("input").val("https://pnrtscr.com/" + Math.random( ).toString(36).substr(2, 6));
+		$("input").val( "https://pnrtscr.com/" + Math.random( ).toString(36).substr(2, 6) );
 	});
-	
 });
